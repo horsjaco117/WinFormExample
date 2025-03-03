97414fdc74d8676a39ae75ea20cb07a7ec7c7750
horsjaco117 <jacob_horsley@live.com>
2025-03-03 08:34:01 -0700

    Added button that affects the text of the form

--- WinFormExample/WinFormExampleForm.vb
+++ WinFormExample/WinFormExampleForm.vb
@@ -4,7 +4,12 @@ Option Explicit On
 Option Strict On
 
 Public Class WinFormExampleForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        ' Me.Text = "Forms are Cool"
 
-    'Code goes here
-
+        Me.Text = StrReverse(Me.Text)
+    End Sub
 End Class
